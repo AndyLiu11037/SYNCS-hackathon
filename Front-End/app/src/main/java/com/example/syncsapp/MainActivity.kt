ffe7b6e86527fba5f1f329e262d3b386f286acbd
jzWang <jzwang2410@gmail.com>
2020-08-29 01:28:42 +1000

    added new leaderboard activity

--- Front-End/app/src/main/java/com/example/syncsapp/MainActivity.kt
+++ Front-End/app/src/main/java/com/example/syncsapp/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.example.syncsapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val playButton = findViewById<Button>(R.id.playButton);
+        playButton.setOnClickListener{
+            Log.d("BUTTON", "The playbutton is being clicked");
+        }
+
+        val leaderboardButton = findViewById<Button>(R.id.leaderboardButton);
+        leaderboardButton.setOnClickListener{
+            openLeaderboard();
+        }
+    }
+
+    fun openLeaderboard() {
+        val intent = Intent(this, LeaderboardActivity::class.java);
+        startActivity(intent);
     }
 }
