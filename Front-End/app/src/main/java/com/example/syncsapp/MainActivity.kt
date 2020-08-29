34c91d15b092c1412773b0bdca5f315d66a5830f
AndyLiu11037 <z5161216@unsw.edu.au>
2020-08-29 17:17:55 +1000

    test http

--- Front-End/app/src/main/java/com/example/syncsapp/MainActivity.kt
+++ Front-End/app/src/main/java/com/example/syncsapp/MainActivity.kt
@@ -5,9 +5,13 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import android.widget.TextView
 import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonArrayRequest
 import com.android.volley.toolbox.JsonObjectRequest
-import kotlinx.android.synthetic.main.activity_main.*
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -29,17 +33,31 @@ class MainActivity : AppCompatActivity() {
     fun openLeaderboard() {
         //val intent = Intent(this, LeaderboardActivity::class.java);
         //startActivity(intent);
-        val url = "https://australia-southeast1-syncs-hackathon.cloudfunctions.net/detect"
-
-        val jsonObjectRequest = JsonObjectRequest(
-            Request.Method.GET, url, null,
+        val queue = Volley.newRequestQueue(this)
+    //https://australia-southeast1-syncs-hackathon.cloudfunctions.net/detect
+        val url = "https://www.google.com/"
+        val textView = findViewById<TextView>(R.id.textView)
+        val stringRequest = StringRequest(Request.Method.GET, url,
             { response ->
+                textView.text = "Response is: ${response.substring(0,500)}"
+            },
+            { textView.text = "Bad" })
+
+       /*val jsonObjectRequest = JsonArrayRequest(
+            Request.Method.GET, url, null,
+            Response.Listener { response ->
+
                 textView.text = "Response: %s".format(response.toString())
             },
-            { error ->
+            Response.ErrorListener{ error ->
                 // TODO: Handle error
+                val textView = findViewById<TextView>(R.id.textView)
+                textView.text = "Bad"
             }
         )
+        */
+
+        queue.add(stringRequest)
 
     }
 }
