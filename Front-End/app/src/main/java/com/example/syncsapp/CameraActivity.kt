b8e38148aa33b72b48f5653d0a2840609faed2be
jzWang <jzwang2410@gmail.com>
2020-08-29 22:53:42 +1000

    request sending added but not working

--- Front-End/app/src/main/java/com/example/syncsapp/CameraActivity.kt
+++ Front-End/app/src/main/java/com/example/syncsapp/CameraActivity.kt
@@ -3,27 +3,31 @@ package com.example.syncsapp
 
 import android.app.Activity
 import android.content.Intent
+import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import android.graphics.Color
 import android.os.Bundle
 import android.os.Environment
 import android.provider.MediaStore
+import android.util.Base64
+import android.util.Log
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import android.widget.Button
 import android.widget.Toast
 import androidx.core.content.FileProvider
 import com.android.volley.Request
+import com.android.volley.Response
 import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.Volley
-import com.google.gson.JsonObject
-import kotlinx.android.synthetic.main.activity_camera.*
 import org.json.JSONObject
+import kotlinx.android.synthetic.main.activity_camera.*
 import java.io.ByteArrayOutputStream
 import java.io.File
 
 private const val REQUEST_CODE = 42;
 private const val FILE_NAME = "PHOTO.jpeg";
-private const val URL = ;
+private const val URL = "https://australia-southeast1-syncs-hackathon.cloudfunctions.net/detect";
 private lateinit var photoFile : File;
 
 class CameraActivity : AppCompatActivity(){
@@ -49,6 +53,19 @@ class CameraActivity : AppCompatActivity(){
                 Toast.makeText(this, "Unable to open camera", Toast.LENGTH_SHORT).show();
             }
         }
+
+        val shapeClick = View.OnClickListener(){
+            if(this.currButton != null){
+                this.currButton!!.background.setTint(Color.parseColor("#E86A92"));
+            }
+
+            this.currButton = it as Button;
+            this.currButton!!.background.setTint(Color.parseColor("#41E2BA"));
+        }
+
+        circleButton.setOnClickListener(shapeClick);
+        parallelButton.setOnClickListener(shapeClick);
+        rectangleButton.setOnClickListener(shapeClick);
     }
 
     private fun getPhotoFile(fileName: String): File {
@@ -60,14 +77,33 @@ class CameraActivity : AppCompatActivity(){
         if(requestCode == REQUEST_CODE && resultCode == Activity.RESULT_OK) {
             val takenImage = BitmapFactory.decodeFile(photoFile.absolutePath);
             imageView.setImageBitmap(takenImage);
-
-            val baos = ByteArrayOutputStream();
-            takenImage.compress();
             imageView.visibility = View.VISIBLE;
-            var jsonObj = JSONObject();
-            jsonObj.put("name", "value");
-            jsonObj.put("shape", "watever button");
-            "$URL/square"
+
+            var baos = ByteArrayOutputStream();
+            takenImage.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+            val encodedImage = Base64.encodeToString(baos.toByteArray(), Base64.DEFAULT);
+
+            val pictureObj = JSONObject();
+            pictureObj.put("image", encodedImage);
+            pictureObj.put( "shape", this.currButton!!.text);
+
+            val request = JsonObjectRequest(
+                Request.Method.POST,
+                URL,
+                pictureObj,
+                Response.Listener<JSONObject> {
+                    response ->
+                    Log.d("CAMERA", "Response object is ${response.toString()}")
+
+                },
+                Response.ErrorListener{
+                    error ->
+                    Log.d("CAMERA","error: ${error.message}")
+                }
+            )
+
+            val queue = Volley.newRequestQueue(this);
+            queue.add(request);
 
         }else{
             super.onActivityResult(requestCode, resultCode, data);
