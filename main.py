04e95ada887f1895623dccedaad2490420c90a63
Andy Liu <andyliu11037@gmail.com>
2020-08-29 22:42:19 +1000

    fix image

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from backend import jong
 import cv2
 import base64
+from PIL import Image
+from io import StringIO
+import numpy as np
+import cv2
 def detect(request):
     feature = 'circles'
     request_json = request.get_json()
@@ -10,8 +14,9 @@ def detect(request):
     except:
         return f'jong World!'
 
-
-    img = cv2.imread(img, 0)
+    pilImage = Image.open(StringIO(img))
+    npImage = np.array(pilImage)
+    img = cv2.fromarray(npImage)
     score, image = jong(img, shape)
     encoded_image = base64.b64encode(image.tobytes())
 
