192a4fc49dcbb2491997e4362bb9134d9fc4a7b1
Andy Liu <andyliu11037@gmail.com>
2020-08-29 22:04:26 +1000

    check

--- main.py
+++ main.py
@@ -3,12 +3,14 @@ import cv2
 import base64
 def detect(request):
     feature = 'circles'
-    img = cv2.imread('./sample_circles/12.jpg', 0)
+    request_json = request.get_json()
+    img = base64.b64decode(request_json['image'])
+    shape = request.json['shape']
+
+    img = cv2.imread(img, 0)
     score, image = jong(img, feature)
     encoded_image = base64.b64encode(image.tobytes())
-    
-    request_json = request.get_json()
-    print(request_json)
+
     if request.args and 'message' in request.args:
         return request.args.get('message')
     elif request_json and 'message' in request_json:
