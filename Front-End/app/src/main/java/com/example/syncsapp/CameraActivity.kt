6463280ca118dff2966f37b0f707b78218007172
jzWang <jzwang2410@gmail.com>
2020-08-29 17:00:58 +1000

    camera design updated

--- Front-End/app/src/main/java/com/example/syncsapp/CameraActivity.kt
+++ Front-End/app/src/main/java/com/example/syncsapp/CameraActivity.kt
@@ -3,7 +3,6 @@ package com.example.syncsapp
 
 import android.app.Activity
 import android.content.Intent
-import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.os.Bundle
 import android.os.Environment
